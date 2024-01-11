283397295fac113386fe85bc0e0efc294fd0fd8b
FaleteRusso <faleterusso89@gmail.com>
2024-01-11 19:48:53 +0100

    hola

--- src/main/java/com/corenetworks/presentacion/cajero/ClienteCajero.java
+++ src/main/java/com/corenetworks/presentacion/cajero/ClienteCajero.java
@@ -1,6 +1,7 @@
 package com.corenetworks.presentacion.cajero;
 
 import com.corenetworks.modelo.CuentaBancaria;
+import com.corenetworks.persistencia.AccesoCuentaBancaria;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,49 +9,59 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.sql.SQLOutput;
+import java.sql.SQLException;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
 
 public class ClienteCajero {
-    private static Scanner sc = new Scanner(System.in);
+    private static Scanner teclado = new Scanner(System.in);
+    private static String cuenta;
+
     public static void main(String[] args) {
-        if(solicitarPin()) {
-            while (true) {
-                CuentaBancaria cB1 = solicitarDatos();
-                if (cB1.getTipoOperacion() == null) {
-                    break;
-                }
-                System.out.println(cB1.toString());
-                try (Socket cl = new Socket("localhost", 3000);) {
-                    PrintWriter mAE = new PrintWriter(cl.getOutputStream(), true);
-                    mAE.println(cB1.getTipoOperacion() + "," + cB1.getId() + "," + cB1.getCantidad());
-                    System.out.println("Esperando respuesta del servidor...");
-                    BufferedReader mR = new BufferedReader(new InputStreamReader(cl.getInputStream()));
-                    System.out.println(mR.readLine());
-                } catch (UnknownHostException e) {
-                    System.out.println(e.toString());
-                } catch (IOException e) {
-                    System.out.println(e.toString());
+
+        try {
+            if (solicitarPIN()) {
+                while (true) {
+                    CuentaBancaria cB1 = solicitarDatos();
+                    if (cB1.getTipoOperacion() == null) {
+                        break;
+                    }
+                    System.out.println(cB1.toString());
+                    try (Socket cliente = new Socket("localhost", 3000);) {
+                        PrintWriter mensajeAEnviar = new PrintWriter(cliente.getOutputStream(), true);
+                        mensajeAEnviar.println(cB1.getTipoOperacion() + "," + cB1.getId() + "," + cB1.getCantidad());
+                        System.out.println("Esperando respuesta del servidor...");
+                        BufferedReader mesajeRecibido = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                        System.out.println(mesajeRecibido.readLine());
+                    } catch (UnknownHostException e) {
+                        System.out.println(e.toString());
+                    } catch (IOException e) {
+                        System.out.println(e.toString());
+                    }
                 }
             }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
         }
     }
 
-    private static boolean solicitarPin() {
-        int contador = 0 ;
-
+    private static boolean solicitarPIN() throws SQLException, ClassNotFoundException {
+        int contador = 0;
         String pin = null;
+        AccesoCuentaBancaria aCB1 = new AccesoCuentaBancaria();
+        System.out.printf("Escribir la cuenta:  ");
+        cuenta = (teclado.nextLine());
         while (true) {
-            System.out.println("escriba su codigo PIN");
-            pin = sc.nextLine();
-            if (pin.equals(("1234"))) {
+            System.out.println("Escriba el PIN: ");
+            pin = teclado.nextLine();
+            if (pin.equals(aCB1.obtenerPin(cuenta))) {
                 return true;
             } else {
-                System.out.println("PIN Incorrecto");
+                System.out.println("PIN INCORRECTO");
                 contador++;
                 if (contador == 3) {
-                    System.out.println("Su tarjeta ha sido bloqueda tras 3 PIN incorrectos");
+                    System.out.println("Recoja su tarjeta en nuestra sucursal.");
                     return false;
                 }
             }
@@ -59,27 +70,30 @@ public class ClienteCajero {
 
     private static CuentaBancaria solicitarDatos() {
         CuentaBancaria cB1 = new CuentaBancaria();
+
         int tipoOperacion = 0;
+
         System.out.printf("%s %n", "-".repeat(50));
-            System.out.printf("%s %n", "MENU BANCARIO");
-            System.out.printf("%s %n", "-".repeat(50));
-            System.out.printf("%s %n", "1) Consultar Saldo");
-            System.out.printf("%s %n", "2) Retirar efectivo");
-            System.out.printf("%s %n", "3) Ingresar efectivo");
-            System.out.printf("%s %n", "4) Salir");
-            System.out.printf("Escriba la opción -> ");
-            tipoOperacion = sc.nextInt();
-            sc.nextLine();
-            if (tipoOperacion == 4) {
-                return cB1;
-            }
-            System.out.printf("Escriba la cuenta -> ");
-            cB1.setId(sc.nextLine());
-            if (tipoOperacion == 2 || tipoOperacion == 3) {
-                System.out.printf("Eescriba la cantidad -> ");
-                cB1.setCantidad(sc.nextDouble());
-            }
-            cB1.setTipoOperacion(Integer.toString(tipoOperacion));
+        System.out.printf("%s %n", "MENU BANCARIO");
+        System.out.printf("%s %n", "-".repeat(50));
+        System.out.printf("%s %n", "1) Consultar Saldo");
+        System.out.printf("%s %n", "2) Retirar una cantidad");
+        System.out.printf("%s %n", "3) Ingresar una cantidad");
+        System.out.printf("%s %n", "4) Salir");
+        System.out.printf("Escriba la opción -> ");
+        tipoOperacion = teclado.nextInt();
+        teclado.nextLine();
+        if (tipoOperacion == 4) {
             return cB1;
         }
+        cB1.setId(cuenta);
+        if (tipoOperacion == 2 || tipoOperacion == 3) {
+            System.out.printf("Escribir la cantidad: ");
+            cB1.setCantidad(teclado.nextDouble());
+        }
+        cB1.setTipoOperacion(Integer.toString(tipoOperacion));
+        return cB1;
+
+
+    }
 }
--- src/main/java/com/corenetworks/presentacion/cajero/ServidorCajero.java
+++ src/main/java/com/corenetworks/presentacion/cajero/ServidorCajero.java
@@ -1,19 +1,23 @@
 package com.corenetworks.presentacion.cajero;
 
+import com.corenetworks.persistencia.AccesoCuentaBancaria;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.Arrays;
 
 public class ServidorCajero {
-    private static int saldo= 3000;
+    private static double saldo;
     public static void main(String[] args) {
         BufferedReader mR = null;
         PrintWriter mAE= null;
         String[] datos=null;
+        AccesoCuentaBancaria aCB= new AccesoCuentaBancaria();
             try(ServerSocket sv= new ServerSocket(3000);){
              while (true) {
                  System.out.println("Esperando peticion...");
@@ -23,24 +27,32 @@ public class ServidorCajero {
                  datos=mR.readLine().split(",");
                  double cantidad = Double.parseDouble(datos[2]);
                  System.out.println(Arrays.toString(datos));
+                 saldo=aCB.solicitarSaldo(datos[1]);
                  mAE=new PrintWriter(s1.getOutputStream(),true);
                  switch (datos[0]){
                      case "1":mAE.println("Su saldo es -> "+saldo);
                      break;
                      case "2":if(saldo>=cantidad){
+                         aCB.actualizarSaldo(datos[1],cantidad*-1);
+                         saldo= aCB.solicitarSaldo(datos[1]);
                          mAE.println("Su saldo es -> "+(saldo-cantidad));
-                         saldo-=cantidad;
                      }else {
                          mAE.println("Saldo insuficiente "+(saldo));
                      }
                      break;
-                     case "3":mAE.println("Su saldo es -> "+(saldo+cantidad));
-                     saldo+=cantidad;
+                     case "3":aCB.actualizarSaldo(datos[1],cantidad);
+                     saldo= aCB.solicitarSaldo(datos[1]);
+                     mAE.println("Su saldo es -> "+(saldo+cantidad));
+
                      break;
                  }
              }
         } catch (IOException e) {
             System.out.println(e.toString());
-        }
+        } catch (SQLException e) {
+                System.out.println(e.toString());
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.toString());
+            }
     }
 }
